3e8cf583415e4b99e181c24dadcb572710162886
veraest <113547514+veraest@users.noreply.github.com>
2023-05-02 00:18:33 +0300

    Add files via upload

--- NavigationHW4updateagain/Navigation/SceneDelegate.swift
+++ NavigationHW4updateagain/Navigation/SceneDelegate.swift
@@ -34,7 +34,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func createProfileViewController() -> UINavigationController {
-        let profileViewController = ProfileViewController()
+        let profileViewController = LogInViewController()
         profileViewController.title = "Профиль"
         profileViewController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person.circle"), tag: 1)
         return UINavigationController(rootViewController: profileViewController)
