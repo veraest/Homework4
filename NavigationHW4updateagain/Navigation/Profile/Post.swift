8158bad4cf7cc22e81c8dea8c7f9b8e380ac2dfc
veraest <113547514+veraest@users.noreply.github.com>
2023-05-03 14:09:57 +0300

    Add files via upload

--- NavigationHW4updateagain/Navigation/Profile/Post.swift
+++ NavigationHW4updateagain/Navigation/Profile/Post.swift
@@ -16,4 +16,28 @@ struct Post {
     let views: Int
 }
 
+var posts: [Post] = [
+   Post(author: "Solar system",
+        description: "Mercury is the first planet of our Solar system.",
+        image: "Mercury",
+        likes: 1232,
+        views: 5967),
 
+   Post(author: "Milkey Way",
+        description: "Venus is too hot for any form of life.",
+        image: "Venus",
+        likes: 487,
+        views: 1506),
+
+   Post(author: "Andromeda",
+        description: "We'll destroy you one day, the Earth.",
+        image: "Earth",
+        likes: 59873,
+        views: 305667),
+
+   Post(author: "NASA",
+        description: "NASA says that the Red Planer is waiting.",
+        image: "Mars",
+        likes: 65,
+        views: 218),
+   ]
